803e1b486acd1e0ab8911e4ce443bb55f7fc3c0c
divyakodenikiri <divyapadpu99@gmail.com>
2022-12-19 15:30:16 +0530

    firstcommit

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class _QuizPageState extends State<QuizPage> {
     'A slug\'s blood is green.?',
   ];
 
+  List<bool> answers = [false,true,true];
+
   int questionNumber = 0;
 
   @override
@@ -71,6 +73,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+
+                bool correctAnswer = answers[questionNumber];
+                if(correctAnswer == true){
+                  print('user got it right');
+                }else{
+                  print('user got it wrong');
+                }
+
+
                 setState(() {
                   questionNumber++;
                 });
@@ -94,6 +105,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                //hello shanu
+
+                 bool correctAnswer = answers[questionNumber];
+                if(correctAnswer == false){
+                  print('user got it right');
+                }else{
+                  print('user got it wrong');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
